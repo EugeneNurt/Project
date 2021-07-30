d1e74cafff23d2b710e9380d2b471160540e3aca
EugeneNurt <e.i.nurtdinov@uspu.su>
2021-07-30 19:04:46 +0300

    comment

---
+++ src/Rita/lift/Lift.ts
@@ -0,0 +1,52 @@
+import Cabin from "./cabin";
+import Button from "./button";
+import LiftLogic from "./liftLogic";
+import СhoiceGame from "../../_Main/СhoiceGame";
+import ExitButton from "../exitButton";
+
+export default class Lift {
+    public cabin: Cabin;
+    public buttons: Button[];
+    public logic: LiftLogic;
+    public floorsCount: number;
+    public scene: PIXI.Container;
+    public exit: ExitButton;
+
+
+    constructor(choiceGame: СhoiceGame) {
+        let a = new PIXI.Sprite(PIXI.Texture.from("./assets/Image/back.jpg"))
+        a.width = screen.width;
+        a.height = screen.height;
+        window.app.stage.addChild(a);
+
+        this.exit = new ExitButton(choiceGame, this);
+
+        this.scene = this.createScene();
+        window.app.stage.addChild(this.scene);
+        this.scene.sortableChildren = true;
+
+        this.floorsCount = 5;
+        this.buttons = this.createButton();
+
+        this.cabin = new Cabin(this);
+        this.logic = new LiftLogic(this);
+    }
+
+
+    createScene(): PIXI.Container {
+        let r = new PIXI.Container();
+        r.width = 500;
+        r.height = 500;
+        r.x = window.app.screen.width / 2 - 250;
+        r.y = window.app.screen.height / 2 - 250;
+        return r;
+    }
+
+    createButton(): Button[] {
+        let result: Button[] = [];
+        for (let i = 0; i < this.floorsCount; i++) {
+            result.push(new Button(this, i))
+        }
+        return result;
+    }
+}
